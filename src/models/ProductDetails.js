cfb1e97d25efe71f9b39356d7aa8316657ad76ff
Shoaib Ahmad <shoaib.ahmad8786@gmail.com>
2025-06-18 14:44:58 +0530

    Update ProductDetails.js

--- src/models/ProductDetails.js
+++ src/models/ProductDetails.js
@@ -1,26 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  ServiceCharge: [  // This holds an array of services and charges
-    {
-      service: {
-        type: String,
-        required: true
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    ServiceCharge: [
+      // This holds an array of services and charges
+      {
+        charge: {
+          type: Number, // Use Number if you're storing numeric values (recommended)
+          required: true,
+        },
       },
-      charge: {
-        type: Number,  // Use Number if you're storing numeric values (recommended)
-        required: true
-      }
-    }
-  ],
+    ],
+    tax: {
+      type: Number,
+      default: 0, // Default tax value is 0
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-}, {
-  timestamps: true
-});
-
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
